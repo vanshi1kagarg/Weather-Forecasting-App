13c93357eaff6800147ec6f5e38e852c21dc6c6f
vanshi1kagarg <you@example.com>
2024-07-21 19:55:26 +0530

    Initial commit. Added Weather page.

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1d6ef58c8fmshe3fdb0fbda19afdp10c36ejsn30b250ef43db',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
